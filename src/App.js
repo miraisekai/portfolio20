56e79a3c446b7cfb6bb25f4d1de7cf75f4fed020
miraisekai <116041477+miraisekai@users.noreply.github.com>
2023-03-24 23:17:12 -0500

    first update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+// import from react
+import { useState } from 'react';
 
+//import components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import Page from './components/Page';
 function App() {
+  const [pages] = useState([
+    {
+      name: 'about me'
+    },
+    {
+      name: 'portfolio'
+    },
+    {
+      name: 'contact'
+    },
+    {
+      name: 'resume'
+    },
+
+  ]);
+
+  const [currentPage, setCurrentPage] = 
+  useState(pages[0]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <Header>
+        <Nav>
+          
+        </Nav>
+      </Header>
+      
+      <Footer/>
+     
+
     </div>
   );
 }
