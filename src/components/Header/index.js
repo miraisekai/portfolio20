ea321b209f58a63b40d98e09094ef715bf4cda0e
miraisekai <116041477+miraisekai@users.noreply.github.com>
2023-04-17 15:10:33 -0500

    fixed links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,16 +7,16 @@ function Header() {
   return (
     <header className="header">
       <h1>Ben Espino</h1>
-      <NavLink className="link" to="/">
+      <NavLink className="link" to="/portfolio20">
         About Me
       </NavLink>
-      <NavLink className="link" to="/portfolio">
+      <NavLink className="link" to="/portfolio20/portfolio">
         Portfolio
       </NavLink>
-      <NavLink className="link" to="/contact">
+      <NavLink className="link" to="/portfolio20/contact">
         Contact
       </NavLink>
-      <NavLink className="link" to="/Resume">
+      <NavLink className="link" to="/portfolio20/resume">
         Resume
       </NavLink>
     </header>
